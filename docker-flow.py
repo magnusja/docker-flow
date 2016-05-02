12c3eaa3de6a29678443de65551feea652bc0eac
jahnen <jahnen@in.tum.de>
2016-05-02 21:51:46 +0200

    Kong API gateway

--- docker-flow.py
+++ docker-flow.py
@@ -1,13 +1,19 @@
 import argparse
+import json
 import logging
 
 import dockerflow
+import kong
 
 logger = logging.getLogger(__name__)
+
 
 def setup_logging():
     logging.basicConfig(level=logging.CRITICAL, format='%(message)s')
     logger = logging.getLogger(dockerflow.__name__)
+    logger.setLevel(logging.DEBUG)
+
+    logger = logging.getLogger(kong.__name__)
     logger.setLevel(logging.DEBUG)
 
     logger = logging.getLogger(__name__)
@@ -22,6 +28,9 @@ def setup_argparser():
     parser.add_argument('--push', help='Push image to registry or docker hub', action='store_true')
     parser.add_argument('--consul-dns', help='Set DNS to container with name consul', action='store_true',
                         dest='consul_dns')
+    parser.add_argument('--kong', help='Kong admin API, e.g. http://localhost:8001')
+    parser.add_argument('--kong-json', help='kong.json file for registering at kong API gateway', default='kong.json')
+    parser.add_argument('--kong-plugin', help='json file to activate a plugin', action='append')
 
     return parser
 
@@ -41,6 +50,10 @@ def main():
             flow.push()
 
     flow.restart_container(args.consul_dns)
+
+    if args.kong:
+        with open(args.kong_json) as kong_json:
+            kong.configure_kong(args.kong, kong_json=json.load(kong_json), plugins=args.kong_plugin)
 
 
 if __name__ == '__main__':
---
+++ kong/kong.py
@@ -0,0 +1,41 @@
+import json
+import logging
+
+import requests
+
+logger = logging.getLogger(__name__)
+
+
+class KongApiGateway(object):
+    def __init__(self, host):
+        self.host = host
+
+    def register_api(self, name, payload):
+        url = self.host + '/apis/'
+
+        logger.info('Deleting API with name %s', name)
+        response = requests.delete(url + name)
+        logger.debug(response.text)
+
+        logger.info('Adding API with name %s', name)
+        response = requests.post(url, json=payload)
+        logger.debug(response.text)
+        assert response.status_code == 201, 'Kong API adding error'
+
+    def add_plugin(self, name, plugin):
+        url = self.host + '/apis/' + name + '/plugins'
+        logger.info('Adding plugin to API with name %s', name)
+        response = requests.post(url, json=plugin)
+        logger.debug(response.text)
+        assert response.status_code == 201, 'Kong API adding error'
+
+
+def configure_kong(host, kong_json, plugins=None):
+    name = kong_json['name']
+    kong = KongApiGateway(host)
+    kong.register_api(name, kong_json)
+
+    if plugins:
+        for plugin in plugins:
+            with open(plugin) as p:
+                kong.add_plugin(name=name, plugin=json.load(p))
